5a7a4e9e64b43d30312d58693f4b702ea2f247b9
BBC <bbc@bbcs-macbook-pro.local>
2017-04-11 13:52:14 +0530

    first commit with login working.

---
+++ Login Practice/SecondVC.swift
@@ -0,0 +1,24 @@
+//
+//  SecondVC.swift
+//  Login Practice
+//
+//  Created by BBC on 06/04/17.
+//  Copyright © 2017 com.bbc.test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondVC: UIViewController {
+    
+    @IBAction func exitButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
--- Login Practice/ViewController.swift
+++ Login Practice/ViewController.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    //MARK: Properties and actions
+    
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+   
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+      
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        print("pressed")
+        if usernameTextField.text == "" || passwordTextField.text == ""
+        {
+            print("fill the fields")
+        }
+        else if usernameTextField.text == passwordTextField.text
+        {
+            print("access granted")
+            let SecondVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondVC")
+            self.present(SecondVC, animated: true, completion: nil)
+            
+        }
+    
+    func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
@@ -23,3 +52,4 @@ class ViewController: UIViewController {
 
 }
 
+}
